ad5b4e74f50d451345dfde0140d2215e5b1df735
Nelson <peternelson063@gmail.com>
2024-05-29 04:55:42 +0100

    updated

--- src/pages/home/ProtectedRoute.jsx
+++ src/pages/home/ProtectedRoute.jsx
@@ -4,9 +4,9 @@ import { Navigate, Route } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = !!localStorage.getItem('token');
 
-  if (!isAuthenticated) {
-    return <Navigate to='/login' replace />;
-  }
+  // if (!isAuthenticated) {
+  //   return <Navigate to='/' replace />;
+  // }
   return children;
 };
 export default ProtectedRoute;
